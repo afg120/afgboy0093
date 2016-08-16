b0041e8141743e70a9c92db8103c30a53e1a4c04
afg120 <alirezanazari582@gmail.com>
2016-08-16 15:55:07 +0200

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {144658773},--Sudo users                               
+    sudo_users = {246027474},--Sudo users                               
   disabled_channels = {246027474},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
